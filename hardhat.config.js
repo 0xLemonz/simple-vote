aa2ad3ab723a2b5a5aeb80fee4a717172c4f9505
Lemonz.eth <lemonzm7@gmail.com>
2025-07-01 18:25:25 +0330

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,17 +2,16 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.0", 
+  solidity: "0.8.0",
   networks: {
     sepolia: {
       url: process.env.ALCHEMY_SEPOLIA_URL || "",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-    },
-    localhost: {
-      url: "http://127.0.0.1:8545",
     }
   },
-  mocha: {
-    timeout: 20000 
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY || "" 
+    }
   }
 };
